f33ac112d0fb3789f8c25eb58f713ba6a9632676
Gareth Grindeland <gt.grindeland@gmail.com>
2023-05-08 15:56:39 -0700

    Add poem form and output
    Co-authored-by: Eusebie Siebenberg <eusebiedev@users.noreply.github.com>

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -5,7 +5,31 @@ namespace MadLibs.Controllers
 {
   public class HomeController : Controller
   {
-    [Route("/")] //can change to "/form" later
+    [Route("/")]
+    public ActionResult HomePage() { return View();}
+
+    [Route("/poemform")]
+    public ActionResult PoemForm() { return View();}
+
+    [Route("/poem")]
+    public ActionResult Poem(string poemnoun1, string poemnoun2, string poemnoun3, string poemnoun4, string poemnoun5, string poemnoun6, string poemadjective1, string poemdirection1, string poemnoun7, string poemdirection2, string bodypart)
+    {
+      PoemVariable myPoemVariable = new PoemVariable();
+      myPoemVariable.PoemNoun1 = poemnoun1;
+      myPoemVariable.PoemNoun2 = poemnoun2;
+      myPoemVariable.PoemNoun3 = poemnoun3;
+      myPoemVariable.PoemNoun4 = poemnoun4;
+      myPoemVariable.PoemNoun5 = poemnoun5;
+      myPoemVariable.PoemNoun6 = poemnoun6;
+      myPoemVariable.PoemNoun7 = poemnoun7;
+      myPoemVariable.PoemAdjective1 = poemadjective1;
+      myPoemVariable.PoemDirection1 = poemdirection1;
+      myPoemVariable.PoemDirection2 = poemdirection2;
+      myPoemVariable.BodyPart = bodypart;
+      return View(myPoemVariable);
+    }
+
+    [Route("/form")] //can change to "/form" later
     public ActionResult Form() { return View(); }
 
     [Route("/story")]
---
+++ MadLibs/Models/PoemVariable.cs
@@ -0,0 +1,20 @@
+namespace MadLibs.Models
+{
+  public class PoemVariable
+  {
+    public string PoemNoun1 { get; set;}
+    public string PoemNoun2 { get; set;}
+    public string PoemNoun3 { get; set;}
+    public string PoemNoun4 { get; set;}
+    public string PoemNoun5 { get; set;}
+    public string PoemNoun6 { get; set;}
+    public string PoemNoun7 { get; set;}
+    public string PoemAdjective1 { get; set;}
+    public string PoemDirection1 { get; set;}
+    public string PoemDirection2 { get; set;}
+
+    public string BodyPart { get; set; }
+
+    
+  }
+}
