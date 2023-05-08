a614ead00aaf642539d1ae1cdceb87ca70761e5d
Gareth Grindeland <gt.grindeland@gmail.com>
2023-05-08 15:09:18 -0700

    add form and story for the first madlib

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -5,15 +5,23 @@ namespace MadLibs.Controllers
 {
   public class HomeController : Controller
   {
-    [Route("/form")]
+    [Route("/")] //can change to "/form" later
     public ActionResult Form() { return View(); }
 
     [Route("/story")]
-    public ActionResult Story(string adjective1, string noun1)
+    public ActionResult Story(string adjective1, string noun1, string name1, string adverb1, string verb1, string exclamation1, string adjective2, string number1, string interjection, string place)
     {
       MadLibsVariable myMadLibsVariable = new MadLibsVariable();
       myMadLibsVariable.Adjective1 = adjective1;
       myMadLibsVariable.Noun1 = noun1;
+      myMadLibsVariable.Name1 = name1;
+      myMadLibsVariable.Adverb1 = adverb1;
+      myMadLibsVariable.Verb1 = verb1;
+      myMadLibsVariable.Exclamation1 = exclamation1;
+      myMadLibsVariable.Adjective2 = adjective2;
+      myMadLibsVariable.Number1 = number1;
+      myMadLibsVariable.Interjection = interjection;
+      myMadLibsVariable.Place = place;
       return View(myMadLibsVariable);
     }
   }
--- MadLibs/Models/MadLibVariable.cs
+++ MadLibs/Models/MadLibVariable.cs
@@ -4,5 +4,16 @@ namespace MadLibs.Models
   {
     public string Adjective1 { get; set;}
     public string Noun1 { get; set;}
+    public string Name1 { get; set;}
+
+    public string Adverb1 { get; set; }
+
+    public string Verb1 { get; set; }
+    public string Exclamation1 { get; set; }
+    public string Adjective2 { get; set;}
+    public string Number1 { get; set;}
+
+    public string Interjection { get; set; }
+    public string Place { get; set; }
   }
 }
