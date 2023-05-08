e57ae924cbb258fac454497de5485430f10a7184
Gareth Grindeland <gt.grindeland@gmail.com>
2023-05-08 14:15:05 -0700

    add home directory and update cshtml files

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -1,10 +1,20 @@
-using MadLibs.Models;
 using Microsoft.AspNetCore.Mvc;
+using MadLibs.Models;
 
 namespace MadLibs.Controllers
 {
   public class HomeController : Controller
   {
+    [Route("/form")]
+    public ActionResult Form() { return View(); }
 
+    [Route("/story")]
+    public ActionResult Story(string adjective1, string noun1)
+    {
+      MadLibsVariable myMadLibsVariable = new MadLibsVariable();
+      myMadLibsVariable.Adjective1 = adjective1;
+      myMadLibsVariable.Noun1 = noun1;
+      return View(myMadLibsVariable);
+    }
   }
 }
--- MadLibs/Models/MadLibVariable.cs
+++ MadLibs/Models/MadLibVariable.cs
@@ -2,6 +2,7 @@ namespace MadLibs.Models
 {
   public class MadLibsVariable
   {
-
+    public string Adjective1 { get; set;}
+    public string Noun1 { get; set;}
   }
 }
